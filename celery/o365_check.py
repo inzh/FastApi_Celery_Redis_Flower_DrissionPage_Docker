9f570ae345f175811b259caa6e5992cd7f70ff76
inzh <inzh@msn.com>
2024-04-18 23:03:43 +0800

    update o365 check

--- celery/o365_check.py
+++ celery/o365_check.py
@@ -25,10 +25,10 @@ def login_check(email, passwd):
     with open("result/check history.txt", "a+", encoding="UTF-8") as writers:
         writers.write(f"{email}:{passwd}" + os.linesep)
     email_pass = f"{email}:{passwd}"
-    new_line = LoginValid(email_pass=email_pass)
-    db = SessionLocal()
     page = None
     try:
+        new_line = LoginValid(email_pass=email_pass)
+        db = SessionLocal()
         email_pass_exist = db.query(LoginValid).filter(LoginValid.email_pass == new_line.email_pass).first()
         db.close()
         if email_pass_exist:
@@ -40,29 +40,35 @@ def login_check(email, passwd):
             passwd = passwd.strip("\n")
             page = ChromiumPage(co)
             page.get("https://outlook.office365.com/mail/", retry=1, timeout=20)
+            time.sleep(5)
             # 获取登录input，然后输入账号
             login_input = page.ele("@type=email")
             login_input.input(f"{email}\n")
-            time.sleep(1)
-            # 如果需要点击组织还是个人账户，则点击
-            if page.ele("#aadTile", timeout=4):
-                double_click_btn = page.ele("#aadTile", timeout=7)
-                double_click_btn.click(by_js=True, timeout=2)
-            if page.ele("#aadTile", timeout=4):
-                double_click_btn = page.ele("#aadTile", timeout=7)
-                double_click_btn.click(by_js=True, timeout=2)
-            time.sleep(1)
-            # 获取输入密码input，然后输入密码
-            passwd_input = page.ele("@type=password")
-            passwd_input.input(f"{passwd}\n")
-            time.sleep(1)
+            time.sleep(5)
+            if page.ele("@type=password"):
+                # 获取输入密码input，然后输入密码
+                passwd_input = page.ele("@type=password")
+                passwd_input.input(f"{passwd}\n")
+            else:
+                # 如果需要点击组织还是个人账户，则点击
+                if page.ele("#aadTile", timeout=5):
+                    double_click_btn = page.ele("#aadTile", timeout=7)
+                    double_click_btn.click(by_js=True, timeout=2)
+                if page.ele("#aadTile", timeout=5):
+                    double_click_btn = page.ele("#aadTile", timeout=7)
+                    double_click_btn.click(by_js=True, timeout=2)
+                # 获取输入密码input，然后输入密码
+                passwd_input = page.ele("@type=password")
+                passwd_input.input(f"{passwd}\n")
+            time.sleep(10)
             # 如果页面存在 不再显示此消息，则表示登录成功
-            if page.ele("@name=DontShowAgain", timeout=7):
+            if page.ele("@name=DontShowAgain", timeout=10):
                 # 获取是否保持登录中的否按钮，然后点击
                 not_btn = page.ele("#idBtn_Back")
                 not_btn.click()
+                time.sleep(10)
                 # 如果不存在邮箱授权，则会报 Something went wrong 错误
-                if page.ele("Something went wrong", timeout=5):
+                if page.ele("Something went wrong", timeout=8):
                     page.quit()
                     return False
                 else:
@@ -72,7 +78,9 @@ def login_check(email, passwd):
             else:
                 # 不存在 不再显示此消息
                 # 可能是直接进入邮箱了，也可能密码错误等其它情况
-                if page.wait.url_change('https://outlook.office365.com/mail/'):
+                # 获取当前网页url
+                page_url = page.url
+                if "outlook.office365.com" in page_url:
                     page.quit()
                     insert_into_db(email_pass)
                     return True
